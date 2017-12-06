3843938803cd3b19ddb248ddbe93225fd2460e5a
sxyazi <sxyazi@gmail.com>
2017-12-06 22:02:26 +0800

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,18 @@ PATH_TEMP='_temp'
 PATH_SOURCE='_source'
 PATH_TEMPLATE='template'
 
+rm -rf $PATH_DIST
 rm -rf $PATH_TEMP
+rm -rf $PATH_SOURCE
+
 mkdir -p $PATH_TEMP/archive
 mkdir -p $PATH_TEMP/article
 
 if [[ $1 != 'clean' ]]; then
-    rm -rf $PATH_SOURCE
-    git clone $1 $PATH_SOURCE
+    git clone $1 $PATH_DIST
+    git clone $2 $PATH_SOURCE
     php index.php $PATH_DIST $PATH_TEMP $PATH_SOURCE $PATH_TEMPLATE
+    cd $PATH_DIST
+    cp -af $PATH_TEMP/* .
+    git push -u $1 master
 fi
-
-rm -rf $PATH_DIST
-mkdir $PATH_DIST
-cp -af $PATH_TEMP/* $PATH_DIST
