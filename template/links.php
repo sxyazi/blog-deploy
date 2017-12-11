a173d9b8a4397e08e0df6d6096fa7c5a2d137aa4
sxyazi <sxyazi@gmail.com>
2017-12-12 01:35:43 +0800

    完善一些信息

--- template/links.php
+++ template/links.php
@@ -2,15 +2,11 @@
 
 <ul class="links">
     <li>
-        <span>谷哥</span>
-        <a href="https://www.google.com" target="_blank">https://www.google.com</a>
+        <span>阿珏博客</span>
+        <a href="https://www.52ecy.cn" target="_blank">https://www.52ecy.cn</a>
     </li>
     <li>
-        <span>鸭姐</span>
-        <a href="https://duckduckgo.com" target="_blank">https://duckduckgo.com</a>
-    </li>
-    <li>
-        <span>馊狗</span>
-        <a href="https://www.sogou.com" target="_blank">https://www.sogou.com</a>
+        <span>刘浩然博客</span>
+        <a href="http://www.lhrsite.com" target="_blank">http://www.lhrsite.com</a>
     </li>
 </ul>
