2d2521b49921e7452107047a8102f5df79115570
sxyazi <sxyazi@gmail.com>
2017-12-12 17:25:09 +0800

    修改关于

--- template/about.php
+++ template/about.php
@@ -1,3 +1,4 @@
+
 # About me
 
 > (๑ŐдŐ)b哇塞，这次改版我的博客终于有了专门的 about 页面了，之前 about 页面还包含友情链接，看起来很尴尬。。。
@@ -13,4 +14,8 @@
 
 **Email** [sxyazi@gmail.com](mailto:sxyazi@gmail.com) · **GitHub** [@sxyazi](https://git.io/33)
 **Telegram** [@sxyazi](https://t.me/sxyazi) · **Twitter** [@sxyazi](https://twitter.com/sxyazi) · **Music** [@sxyazi](http://music.163.com/#/user/home?id=248687840)
+
+
+
+有人冒充我的名字，开始我也注意到了，不过没怎么关心，后来有些朋友也注意到了跟我说，所以我搞了个常用公开账号的白名单：[Gist](https://gist.github.com/sxyazi/da8ffc01b6bb4d295e6f4f512b6e567b) 。我不想谴责盗用我名字的人，因为这是没有意义的行为，我搞这个白名单只是想证明列表中的这些账号是「我」、「这个三咲亚子」本人。
 
